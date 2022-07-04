56ac01a1e457d24fd5b0ba93525bcfb091b41e40
otabek <splabdev@rfalab.com>
2022-07-04 10:16:43 +0500

    added getting & creating artist

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
 import { PersistGate } from "redux-persist/integration/react"
-import { Web3ReactProvider } from "@web3-react/core";
 import { QueryClientProvider } from 'react-query';
 import { queryClient } from './constants/queryClients'
 import AlertProvider from "./providers/AlertProvider"
@@ -11,24 +10,16 @@ import Router from "./router"
 import { persistor, store } from "./store"
 import "./i18next"
 import { Suspense } from "react"
-import { ethers } from "ethers";
 import PageFallback from "./components/PageFallback"
 import MergedLayout from "./layouts/MergedLayout"
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
-const getLibrary = (provider) => {
-  const library = new ethers.providers.Web3Provider(provider);
-  library.pollingInterval = 8000; // frequency provider is polling
-  return library;
-};
 function App() {
-
   return (
     <Suspense fallback={<PageFallback />} >
       <div className="App">
         <Provider store={store}>
-          <Web3ReactProvider getLibrary={getLibrary}>
             <PersistGate persistor={persistor}>
               <QueryClientProvider client={queryClient}>
                 <MaterialUIProvider>
@@ -43,7 +34,6 @@ function App() {
                 </MaterialUIProvider>
               </QueryClientProvider>
             </PersistGate>
-          </Web3ReactProvider>
         </Provider>
       </div>
     </Suspense>
