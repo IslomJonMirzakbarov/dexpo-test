4ba583b8c5b2a55d29572dd6f80c88faf607b6dc
otabek <splabdev@rfalab.com>
2022-07-12 14:30:32 +0500

    collections fixed

--- src/views/Home/NFTCollections/index.jsx
+++ src/views/Home/NFTCollections/index.jsx
@@ -1,59 +1,98 @@
-import { Box, Button, Container, Typography } from '@mui/material'
+import { Box, Container, Grid, Typography } from '@mui/material'
 import React from 'react'
 import styles from './style.module.scss'
 import Slider from 'react-slick'
 import NFTCard from '../../../components/NFTCard'
-import { NavLink } from 'react-router-dom'
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
+import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
 import { fakeNFTs } from '../../../constants/faker'
+import classNames from 'classnames'
+
+const slidesToShow = 4
 
 const data = [
     {
-        title: "🔥 Hottest Artwork",
-        collections: fakeNFTs
+        title: "Hottest Artwork",
+        collections: [
+            {
+                ...fakeNFTs[0],
+                buttonVariant: 'containedSecondary1',
+            },
+            {
+                ...fakeNFTs[1],
+                buttonVariant: 'containedSecondary',
+                liked: true
+            },
+            {
+                ...fakeNFTs[2],
+                buttonVariant: 'containedSecondary1',
+            },
+        ]
     },
     {
-        title: "⚜️ Notable Artwork",
+        title: "Notable Artwork",
         collections: fakeNFTs
     }
 ]
-
 
 const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 1
-  };
+    slidesToShow,
+    slidesToScroll: 1,
+    prevArrow: <ArrowBackIosNewRoundedIcon />,
+    nextArrow: <ArrowForwardIosRoundedIcon />,
+};
+
+const arrowStyle = {
+
+}
 
 const NFTCollections = () => {
     return (
-        <Box className={styles.container}>
+        <Box className={classNames(styles.container,'collections')}>
             <Container>
                 {
                     data.map((item, idx) => 
                         <Box className={styles.block} key={idx}>
                             <Box 
                                 display="flex" 
-                                justifyContent="space-between"
+                                justifyContent="center"
                                 alignItems="center"
+                                mb={5}
                             >
                                 <Typography variant='h2'>{item.title}</Typography>
-                                <NavLink to={`/marketplace/12`}>
-                                    <Button variant="outlined">More &nbsp;&nbsp;<ArrowForwardIcon/></Button>
-                                </NavLink>
                             </Box>
                             <Box className={styles.collection}>
-                                <Slider {...settings}>
-                                    {
-                                        item.collections.map((card, c) => 
-                                            <div className={styles.card} key={c}>
-                                                <NFTCard {...card}/>
-                                            </div>    
-                                        )
-                                    }
-                                </Slider>
+                                {
+                                    item.collections.length < 4 ?
+                                    <Grid 
+                                        container 
+                                        display='flex' 
+                                        justifyContent="center"
+                                        spacing={3}
+                                        mb={10}
+                                    >
+                                        {
+                                            item.collections.map((card, c) => 
+                                                <Grid item key={c}>
+                                                    <NFTCard {...card}/>
+                                                </Grid>
+                                            )
+                                        }
+                                    </Grid>
+                                    :
+                                    <Slider {...settings}>
+                                        {
+                                            item.collections.map((card, c) => 
+                                                <div className={styles.card} key={c}>
+                                                    <NFTCard {...card}/>
+                                                </div>    
+                                            )
+                                        }
+                                    </Slider>
+                                }
                             </Box>
                         </Box>
                     )
