e8057b22da724f512d9703dfe13d36b1454f9827
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2022-07-20 16:53:54 +0900

    updated

--- src/hooks/useArtistAPI.jsx
+++ src/hooks/useArtistAPI.jsx
@@ -9,7 +9,6 @@ const createArtist = (data, token) =>
 
 const useArtistAPI = ({ isDetail, onSuccess }) => {
   const { token } = useSelector((store) => store.auth);
-  console.log(token);
   const dispatch = useDispatch();
 
   const getArtist = (token) =>
