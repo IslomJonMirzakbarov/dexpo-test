7c964fdbe16cff8bd47489db3fa7ac5ae86f6881
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2022-07-27 13:59:55 +0900

    settings icon added and hovered state optimized as well

--- src/views/MyPage/index.jsx
+++ src/views/MyPage/index.jsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import useCollectionAPI from "../../hooks/useCollectionApi";
+
+import PageSettingsIcon from "/src/assets/icons/page-settings-icon.svg?component";
+
+import styles from "./style.module.scss";
 
 const MyPage = () => {
   const { collections } = useCollectionAPI({
@@ -8,18 +12,17 @@ const MyPage = () => {
     orderBy: "desc",
     size: 10,
   });
+  const [hovered, setHovered] = useState(false);
   console.log(collections?.data?.items);
   return (
-    <div
-      style={{
-        width: "100vh",
-        height: "80vh",
-        display: "flex",
-        justifyContent: "center", // just for demo, later will be replaced with actual code
-        alignItems: "center",
-      }}
-    >
-      MyPage
+    <div className={styles.Container}>
+      <div className={styles.SettingsIconContainer}>
+        <PageSettingsIcon
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+          fill={hovered ? "#7D8890" : "#D1D1D1"}
+        />
+      </div>
     </div>
   );
 };
