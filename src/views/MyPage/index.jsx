51ccca9abace888f482c95a78de258aa471f347c
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2022-07-27 15:19:41 +0900

    added profile section #35

--- src/views/MyPage/index.jsx
+++ src/views/MyPage/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import useCollectionAPI from "../../hooks/useCollectionApi";
 
 import PageSettingsIcon from "/src/assets/icons/page-settings-icon.svg?component";
+import ProfileImageIcon from "/src/assets/icons/profile-img-icon.svg?component";
 
 import styles from "./style.module.scss";
 
@@ -22,6 +23,19 @@ const MyPage = () => {
           onMouseLeave={() => setHovered(false)}
           fill={hovered ? "#7D8890" : "#D1D1D1"}
         />
+      </div>
+
+      <div className={styles.ProfileSection}>
+        <ProfileImageIcon />
+        <div className={styles.UserName}>UserName</div>
+        <div className={styles.WalletAddress}>0x123s…0D7a</div>
+        <div className={styles.Bio}>Bio</div>
+        <div className={styles.BioDescription}>
+          0xA66FD7138A258D4bb689e8CdfC00114e3e6D682E 0xA66FD7138A258D4bb6
+          89e8CdfC00114e3e6D682E 0xA66FD7138A258D4bb689e8CdfC00114e3e6D682E
+          0xA66FD7138A258D4bb689e8CdfC00114e3e6D682E
+          0xA66FD7138A258D4bb689e8CdfC00114e3e6D682E 0xA66FD7138A258D4bb6
+        </div>
       </div>
     </div>
   );
