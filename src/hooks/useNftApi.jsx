03005ca292e5e86f408f1182629ec8556b997b05
otabek <splabdev@rfalab.com>
2022-07-28 14:32:22 +0500

    check added

--- src/hooks/useNftApi.jsx
+++ src/hooks/useNftApi.jsx
@@ -1,32 +1,67 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import { securedAPI } from "../services/api";
-import { useMutation } from "react-query";
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { securedAPI } from '../services/api'
+import { useMutation, useQuery } from 'react-query'
 
-const useNftAPI = ({ onSuccess }) => {
-  const [metaData, setMetaData] = useState('');
-  const { token } = useSelector((store) => store.auth);
-  // console.log(token);
+const size = 10
 
-  const config = {
-    headers: { "content-type": "multipart/form-data" },
-  };
-  const createMetaData = (formData, token) =>
-    securedAPI(token)
-      .post("/api/nft/generateMetadata", formData, config)
-      .then((res) => {
-        setMetaData(res.data);
-        return res.data;
-      });
+const getList = ({ type, page, orderBy = 'desc' },token) => securedAPI(token).post(`/api/nft/list?type=${type}&page=${page}&orderBy=${orderBy}&size=${size}`).then(res => res.data)
 
-  const mutation = useMutation((data) => createMetaData(data, token), {
-    onSuccess,
-  });
+const getDetail = ({ contactAddress, tokenId },token) => securedAPI(token).get(`/api/nft/detail?contact_address${contactAddress}&token_id=${tokenId}`).then(res => res.data)
 
-  return {
-    create: mutation,
-    metadata: metaData,
-  };
-};
+const fetchLike = (data, token) => securedAPI(token).post(`/api/nft/like`, data)
 
-export default useNftAPI;
+const fetchUnlike = (data, token) => securedAPI(token).post(`/api/nft/dislike`, data)
+
+const useNftAPI = ({ 
+    isGetList = false, 
+    isGetDetail = false , 
+    type = 'COLLECTED', 
+    page = 1, 
+    orderBy = 'desc'
+}) => {
+    const { token } = useSelector(store => store.auth)
+
+    const { 
+        data: list, 
+        refetch: refetchList, 
+        isLoading: loadingList, 
+        error 
+    } = useQuery(
+        'get-nft-list',
+        () => getList({ type, page, orderBy },token),{
+        enabled: !!isGetList
+    })
+    
+    const { 
+        data: detail, 
+        refetch: refetchDetail, 
+        isLoading: loadingDetail, 
+        error: errorDetail 
+    } = useQuery(
+        `get-nft-detail`,
+        (payload) => getDetail(payload, token),{
+        enabled: !!isGetDetail
+    })
+
+    const mutationLike = useMutation((data) => fetchLike(data, token),{
+    })
+
+    const mutationDislike = useMutation((data) => fetchUnlike(data, token),{
+    })
+
+    return {
+        postLike: mutationLike,
+        postDislike: mutationDislike,
+        detail,
+        refetchDetail,
+        loadingDetail,
+        errorDetail,
+        list,
+        refetchList,
+        loadingList,
+        error
+    }
+}
+
+export default useNftAPI
