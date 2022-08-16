71357276e338353587b5f0ba6fe30371e6b401d4
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2022-08-16 15:39:45 +0900

    collection logo fixed in my application #62

--- src/views/MyPage/MyApplicationBottom/CollectionStatusTable/index.jsx
+++ src/views/MyPage/MyApplicationBottom/CollectionStatusTable/index.jsx
@@ -7,48 +7,57 @@ import useCollectionAPI from "../../../../hooks/useCollectionApi";
 import styles from "./style.module.scss";
 
 const CollectionStatusTable = () => {
-   const { collections } = useCollectionAPI({
-      isDetail: true,
-      page: 1,
-      orderBy: "desc",
-      size: 200,
-   });
+  const { collections } = useCollectionAPI({
+    isDetail: true,
+    page: 1,
+    orderBy: "desc",
+    size: 200,
+  });
+  return (
+    <table className={styles.Table}>
+      <thead className={styles.TableHead}>
+        <tr className={styles.TableHeadRow}>
+          <th>Collection logo & Name</th>
+          <th>Status</th>
+          <th>Date</th>
+        </tr>
+      </thead>
 
-   return (
-      <table className={styles.Table}>
-         <thead className={styles.TableHead}>
-            <tr className={styles.TableHeadRow}>
-               <th>Collection logo & Name</th>
-               <th>Status</th>
-               <th>Date</th>
-            </tr>
-         </thead>
-
-         <tbody className={styles.TableBody}>
-            {collections?.data?.items.length > 0 &&
-               collections?.data?.items.map((item) => (
-                  <tr className={styles.TableBodyRow}>
-                     <td>
-                        <CollectionStatusSvg className={styles.Svg} />
-                        {item.name}
-                     </td>
-                     <td
-                        className={classNames(
-                           styles.UnderReview,
-                           { [styles.Approved]: item?.status === "COMPLETE" },
-                           { [styles.Rejected]: item?.status === "REJECT" }
-                        )}
-                     >
-                        {item.status}
-                     </td>
-                     <td>
-                        {moment(item.created_at).format("YYYY.MM.DD hh:mm:ss")}
-                     </td>
-                  </tr>
-               ))}
-         </tbody>
-      </table>
-   );
+      <tbody className={styles.TableBody}>
+        {collections?.data?.items.length > 0 &&
+          collections?.data?.items.map((item) => {
+            const itemStatus =
+              item.status === "IDLE" || item.status === "PENDING"
+                ? "Under Review"
+                : item.status === "REJECT"
+                ? "Rejected"
+                : "Approved";
+            return (
+              <tr className={styles.TableBodyRow}>
+                <td>
+                  <img
+                    src={item?.logo_url}
+                    alt="collection logo"
+                    className={styles.Svg}
+                  />
+                  {item.name}
+                </td>
+                <td
+                  className={classNames(
+                    styles.UnderReview,
+                    { [styles.Approved]: item?.status === "COMPLETE" },
+                    { [styles.Rejected]: item?.status === "REJECT" }
+                  )}
+                >
+                  {itemStatus}
+                </td>
+                <td>{moment(item.created_at).format("YYYY.MM.DD hh:mm:ss")}</td>
+              </tr>
+            );
+          })}
+      </tbody>
+    </table>
+  );
 };
 
 export default CollectionStatusTable;
