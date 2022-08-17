de5231c836f42f67da6fc874da21103ca5bdf2c6
otabek <splabdev@rfalab.com>
2022-08-17 17:29:00 +0500

    pdf bugs fixed

--- src/views/Home/NFTCollections/index.jsx
+++ src/views/Home/NFTCollections/index.jsx
@@ -13,7 +13,7 @@ const slidesToShow = 4;
 
 const settings = {
   dots: true,
-  infinite: true,
+  infinite: false,
   speed: 500,
   slidesToShow,
   slidesToScroll: 1,
@@ -76,18 +76,17 @@ const NFTCollections = ({ collections, hottestCollections }) => {
                 {collections?.map(({ nft, artist, market, collection }, c) => (
                   <div className={styles.card} key={c}>
                     <NFTCard
+                      className={styles.card_item}
                       img={nft.token_image}
                       name={nft.token_name}
-                      price={nft.token_name}
+                      price={market?.price}
                       startDate={market?.start_date}
                       endDate={market?.end_date}
-                      leftDays={null}
                       artistName={artist.artist_name}
                       description={nft.token_name}
                       priceType={priceTypeChar?.[market?.type]}
                       hasAction={!!market?.price}
                       purchaseCount={nft.like_count}
-                      isDefault
                       onClick={() =>
                         navigate(
                           `/marketplace/${nft.token_id}/${collection?.contract_address}`
@@ -157,9 +156,10 @@ const NFTCollections = ({ collections, hottestCollections }) => {
                 {collections?.map(({ nft, artist, market, collection }, c) => (
                   <div className={styles.card} key={c}>
                     <NFTCard
+                      className={styles.card_item}
                       img={nft.token_image}
                       name={nft.token_name}
-                      price={nft.token_name}
+                      price={market?.price}
                       startDate={market?.start_date}
                       endDate={market?.end_date}
                       artistName={artist.artist_name}
