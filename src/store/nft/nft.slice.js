4cc54a267761ee45b02901845641e0453e8f0288
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2022-08-25 16:28:34 +0900

    nft storage cleaned

--- src/store/nft/nft.slice.js
+++ src/store/nft/nft.slice.js
@@ -3,20 +3,13 @@ import { createSlice } from "@reduxjs/toolkit";
 export const { actions: nftActions, reducer: nftReducer } = createSlice({
   name: "nft",
   initialState: {
-    likedNfts: [],
     newNftSrc: "",
   },
   reducers: {
-    setLikedNfts(state, { payload }) {
-      state.likedNfts.push(payload);
-    },
-    setDislikedNfts(state, { payload }) {
-      state.likedNfts = state.likedNfts.filter((item) => item !== payload);
-    },
     setNewNftSrc(state, { payload }) {
       state.newNftSrc = payload;
     },
   },
 });
 
-export const { setLikedNfts, setDislikedNfts, setNewNftSrc } = nftActions;
+export const { setNewNftSrc } = nftActions;
