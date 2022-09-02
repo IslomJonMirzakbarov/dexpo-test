4a807e26eb3e63cba2c964edd0e3d21c8187bd70
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2022-09-02 16:51:08 +0900

    user guide link added to the footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,124 +1,125 @@
-import { Box, Container, Grid, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import React from 'react';
-import YoutubeIcon from '../../assets/icons/youtube.svg?component';
-import FacebookIcon from '../../assets/icons/facebook.svg?component';
-import DiscordIcon from '../../assets/icons/discord.svg?component';
-import TelegramIcon from '../../assets/icons/telegram.svg?component';
-import logoImg from '../../assets/images/logo.svg';
-import { NavLink } from 'react-router-dom';
-import styles from './style.module.scss';
-import { useSelector } from 'react-redux';
+import { Box, Container, Grid, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import React from "react";
+import YoutubeIcon from "../../assets/icons/youtube.svg?component";
+import FacebookIcon from "../../assets/icons/facebook.svg?component";
+import DiscordIcon from "../../assets/icons/discord.svg?component";
+import TelegramIcon from "../../assets/icons/telegram.svg?component";
+import logoImg from "../../assets/images/logo.svg";
+import { NavLink } from "react-router-dom";
+import styles from "./style.module.scss";
+import { useSelector } from "react-redux";
+import PDF from "../../assets/docs/UserGuide.pdf";
 
 const list = [
   {
-    title: 'Marketplace',
+    title: "Marketplace",
     children: [
       {
-        title: 'Art',
-        link: '/marketplace'
-      }
-    ]
+        title: "Art",
+        link: "/marketplace",
+      },
+    ],
   },
   {
-    title: 'My Account',
+    title: "My Account",
     children: [
       {
-        title: 'Profile',
-        link: '/user/my-page',
-        isAuthenticated: true
+        title: "Profile",
+        link: "/user/my-page",
+        isAuthenticated: true,
       },
       {
-        title: 'My Collections',
-        link: '/user/collections',
-        isAuthenticated: true
+        title: "My Collections",
+        link: "/user/collections",
+        isAuthenticated: true,
       },
       {
-        title: 'My Application',
-        link: '/user/my-page/artist-status',
-        isAuthenticated: true
+        title: "My Application",
+        link: "/user/my-page/artist-status",
+        isAuthenticated: true,
       },
       {
-        title: 'Favourites',
-        link: '/user/my-page/favorites',
-        isAuthenticated: true
+        title: "Favourites",
+        link: "/user/my-page/favorites",
+        isAuthenticated: true,
       },
       {
-        title: 'Settings',
-        link: '/user/settings',
-        isAuthenticated: true
-      }
-    ]
+        title: "Settings",
+        link: "/user/settings",
+        isAuthenticated: true,
+      },
+    ],
   },
   {
-    title: 'Resources',
+    title: "Resources",
+    children: [
+      // {
+      //   title: 'Help Center',
+      //   link: '/help'
+      // },
+      {
+        title: "User Guide",
+        link: "/user-guide",
+      },
+      {
+        title: "Rankings",
+        link: "/rankings",
+      },
+    ],
+  },
+  {
+    title: "Company",
     children: [
       {
-        title: 'Help Center',
-        link: '/help'
+        title: "About",
+        link: "/about",
       },
-      {
-        title: 'Docs',
-        link: '/docs'
-      },
-      {
-        title: 'Rankings',
-        link: '/rankings'
-      }
-    ]
+    ],
   },
   {
-    title: 'Company',
-    children: [
-      {
-        title: 'About',
-        link: '/about'
-      }
-    ]
-  },
-  {
-    title: 'Community',
+    title: "Community",
     icons: true,
     children: [
       {
-        title: 'Youtube',
-        link: '/youtube',
-        icon: <YoutubeIcon />
+        title: "Youtube",
+        link: "/youtube",
+        icon: <YoutubeIcon />,
       },
       {
-        title: 'Facebook',
-        link: '/facebook',
-        icon: <FacebookIcon />
+        title: "Facebook",
+        link: "/facebook",
+        icon: <FacebookIcon />,
       },
       {
-        title: 'Telegram',
-        link: '/telegram',
-        icon: <TelegramIcon />
+        title: "Telegram",
+        link: "/telegram",
+        icon: <TelegramIcon />,
       },
       // {
       //     title: 'Kakaotalk',
       //     link: '/kakaotalk'
       // },
       {
-        title: 'Discord',
-        link: '/discrod',
-        icon: <DiscordIcon />
-      }
-    ]
-  }
+        title: "Discord",
+        link: "/discrod",
+        icon: <DiscordIcon />,
+      },
+    ],
+  },
 ];
 
 const useStyles = makeStyles((theme) => ({
   grid: {
-    [theme.breakpoints.down('sm')]: {
-      marginTop: 10
-    }
+    [theme.breakpoints.down("sm")]: {
+      marginTop: 10,
+    },
   },
   wrapper: {
-    [theme.breakpoints.down('sm')]: {
-      marginTop: 0
-    }
-  }
+    [theme.breakpoints.down("sm")]: {
+      marginTop: 0,
+    },
+  },
 }));
 
 const Footer = () => {
@@ -149,19 +150,28 @@ const Footer = () => {
                   </Typography>
                   {!item.icons ? (
                     <ul className={styles.links}>
-                      {item.children.map((link) => (
-                        <li key={link.link}>
-                          <NavLink
-                            to={
-                              !token && link.isAuthenticated
-                                ? '/login'
-                                : link.link
-                            }
-                          >
-                            {link.title}
-                          </NavLink>
-                        </li>
-                      ))}
+                      {item.children.map((link) => {
+                        const showPDF = link.link === "/user-guide";
+                        return (
+                          <li key={link.link}>
+                            {showPDF ? (
+                              <a href={PDF} target="_blank" rel="noreferrer">
+                                User Guide
+                              </a>
+                            ) : (
+                              <NavLink
+                                to={
+                                  !token && link.isAuthenticated
+                                    ? "/login"
+                                    : link.link
+                                }
+                              >
+                                {link.title}
+                              </NavLink>
+                            )}
+                          </li>
+                        );
+                      })}
                     </ul>
                   ) : (
                     <ul className={styles.links_icons}>
