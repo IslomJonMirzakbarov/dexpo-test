31e8d05082943bc1ab7e4e24323c5bb3c1faf217
otabek <splabdev@rfalab.com>
2022-10-13 14:00:39 +0500

    fixed blank issue

--- src/providers/MaterialUIProvider.jsx
+++ src/providers/MaterialUIProvider.jsx
@@ -1,14 +1,14 @@
-import { LocalizationProvider } from "@mui/lab";
-import ThemeConfig from "../theme/index";
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import ThemeConfig from '../theme/index';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
-const MaterialUIProvider = ({children}) => {
-  const theme = "light"
+const MaterialUIProvider = ({ children }) => {
+  const theme = 'light';
 
   return (
-    <div className={theme === "dark" ? 'night-mode' : ''} >
-      <ThemeConfig >
-        <LocalizationProvider dateAdapter={AdapterDateFns} >
+    <div className={theme === 'dark' ? 'night-mode' : ''}>
+      <ThemeConfig>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
           {children}
         </LocalizationProvider>
       </ThemeConfig>
