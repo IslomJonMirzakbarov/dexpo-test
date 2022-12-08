a638266eeeeebbaa2035c87a8fda2fd241062fa1
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2022-12-08 20:37:23 +0500

    swap link added instead of faucet

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,11 +1,11 @@
-import TelegramIcon from '../assets/icons/telegram-icon.svg?component';
-import KakaoTalkIcon from '../assets/icons/kakaotalk.svg?component';
-import PersonIcon from '@mui/icons-material/Person';
-import GridViewRoundedIcon from '@mui/icons-material/GridViewRounded';
-import InsertDriveFileRoundedIcon from '@mui/icons-material/InsertDriveFileRounded';
-import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded';
-import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
-import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import TelegramIcon from "../assets/icons/telegram-icon.svg?component";
+import KakaoTalkIcon from "../assets/icons/kakaotalk.svg?component";
+import PersonIcon from "@mui/icons-material/Person";
+import GridViewRoundedIcon from "@mui/icons-material/GridViewRounded";
+import InsertDriveFileRoundedIcon from "@mui/icons-material/InsertDriveFileRounded";
+import FavoriteRoundedIcon from "@mui/icons-material/FavoriteRounded";
+import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
 
 export const pages = [
   {
@@ -38,8 +38,8 @@ export const pages = [
     to: "/rankings",
   },
   {
-    name: "Faucet",
-    to: "/faucet",
+    name: "Swap",
+    to: "/swap",
     isAuthenticated: true,
   },
 ];
@@ -47,57 +47,57 @@ export const pages = [
 export const responsivePages = (handleLogout) => [
   ...pages,
   {
-    name: 'Profile',
+    name: "Profile",
     children: [
       {
-        name: 'Profile',
-        to: '/user/my-page',
-        icon: <PersonIcon />
+        name: "Profile",
+        to: "/user/my-page",
+        icon: <PersonIcon />,
       },
       {
-        name: 'My Collections',
-        to: '/user/collections',
-        icon: <GridViewRoundedIcon />
+        name: "My Collections",
+        to: "/user/collections",
+        icon: <GridViewRoundedIcon />,
       },
       {
-        name: 'My Application',
-        to: '/user/my-page/artist-status',
-        icon: <InsertDriveFileRoundedIcon />
+        name: "My Application",
+        to: "/user/my-page/artist-status",
+        icon: <InsertDriveFileRoundedIcon />,
       },
       {
-        name: 'Favorites',
-        to: '/user/my-page/favorites',
-        icon: <FavoriteRoundedIcon />
+        name: "Favorites",
+        to: "/user/my-page/favorites",
+        icon: <FavoriteRoundedIcon />,
       },
       {
-        name: 'Settings',
-        to: '/user/settings',
-        icon: <SettingsRoundedIcon />
+        name: "Settings",
+        to: "/user/settings",
+        icon: <SettingsRoundedIcon />,
       },
       {
-        name: 'Logout',
+        name: "Logout",
         to: null,
         icon: <LogoutRoundedIcon />,
-        onClick: handleLogout
-      }
-    ]
-  }
+        onClick: handleLogout,
+      },
+    ],
+  },
 ];
 
 export const priceType = {
   FIXED: {
-    key: 'fixed',
-    value: 'Fixed'
+    key: "fixed",
+    value: "Fixed",
   },
   AUCTION: {
-    key: 'auction',
-    value: 'Auction'
-  }
+    key: "auction",
+    value: "Auction",
+  },
 };
 
 export const priceTypeChar = {
-  F: 'Fixed',
-  A: 'Auction'
+  F: "Fixed",
+  A: "Auction",
 };
 
-export const DATE_FORMAT = 'yyyy-MM-DD HH:mm:ss';
+export const DATE_FORMAT = "yyyy-MM-DD HH:mm:ss";
--- src/layouts/MergedLayout/LinkList/index.jsx
+++ src/layouts/MergedLayout/LinkList/index.jsx
@@ -1,14 +1,14 @@
-import { ListItem, List, Box, Typography } from '@mui/material';
-import React, { useRef, useState } from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
+import { ListItem, List, Box, Typography } from "@mui/material";
+import React, { useRef, useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 
-import { pages } from '../../../constants';
-import styles from '../../AuthLayout/style.module.scss';
-import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
-import { useSelector } from 'react-redux';
-import ProfileMenu from '../ProfileMenu';
-import { useOnClickOutside } from '../../../hooks/useOnOutsideClick';
-import classNames from 'classnames';
+import { pages } from "../../../constants";
+import styles from "../../AuthLayout/style.module.scss";
+import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
+import { useSelector } from "react-redux";
+import ProfileMenu from "../ProfileMenu";
+import { useOnClickOutside } from "../../../hooks/useOnOutsideClick";
+import classNames from "classnames";
 
 const LinkList = () => {
   const { pathname } = useLocation();
@@ -26,7 +26,7 @@ const LinkList = () => {
       {pages.map((page) => (
         <ListItem
           className={classNames(styles.navItem, {
-            [styles.active]: pathname.includes(page.to)
+            [styles.active]: pathname.includes(page.to),
           })}
           key={page.name}
         >
@@ -38,17 +38,23 @@ const LinkList = () => {
                 alignItems="center"
                 onClick={handleToggleContact}
               >
-                {page.name}&nbsp;{' '}
+                {page.name}&nbsp;{" "}
                 {!!page.children && <KeyboardArrowDownRoundedIcon />}
               </Typography>
               {!!page.children && contactOpen && (
                 <ProfileMenu options={page.children} />
               )}
             </Box>
-          ) : (
-            <NavLink to={page.isAuthenticated && !token ? '/login' : page.to}>
+          ) : page.name === "Swap" && page.isAuthenticated && token ? (
+            <a href="https://swap.conun.io/" target="_blank" rel="noreferrer">
               <Typography variant="body2" display="flex" alignItems="center">
-                {page.name}&nbsp;{' '}
+                {page.name}&nbsp;{" "}
+              </Typography>
+            </a>
+          ) : (
+            <NavLink to={page.isAuthenticated && !token ? "/login" : page.to}>
+              <Typography variant="body2" display="flex" alignItems="center">
+                {page.name}&nbsp;{" "}
               </Typography>
             </NavLink>
           )}
