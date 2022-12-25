b7e0510c1de4bf3fa868f6bc2a7fa4cf6f634a34
sjss17647 <sjss17647@gmail.com>
2022-12-25 09:46:38 +0500

    banner

--- src/views/Home/Hero/index.jsx
+++ src/views/Home/Hero/index.jsx
@@ -1,20 +1,49 @@
-import { Box, Container } from '@mui/material';
-import React from 'react';
-import styles from './style.module.scss';
-import CarouselItem from './CarouselItem';
-import Slider from 'react-slick';
-import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
-import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
-import classNames from 'classnames';
+import { Box, Container } from '@mui/material'
+import React, { useRef, useState } from 'react'
+import styles from './style.module.scss'
+import CarouselItem from './CarouselItem'
+import Slider from 'react-slick'
+import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded'
+import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded'
+import classNames from 'classnames'
+import banner1 from '../../../assets/images/hero/banner1.jpg'
+import banner2 from '../../../assets/images/hero/banner2.jpg'
+import banner3 from '../../../assets/images/hero/banner3.jpg'
+import banner1Mobile from '../../../assets/images/hero/banner1Mobile.jpg'
+import banner2Mobile from '../../../assets/images/hero/banner2Mobile.jpg'
+import banner3Mobile from '../../../assets/images/hero/banner3Mobile.jpg'
 
 var items = [
   {
-    pretitle: 'Create, sell and collect',
-    name: 'Extraordinary NFTs',
-    description:
-      'Great chance for artists to create their own items. Lowest fee for selling and buying NFTs'
+    pretitle: '작가_미상',
+    title: '백자철화용문호',
+    titleColor: '#fff',
+    pretitleColor: '#EAEFF3',
+    image: banner1,
+    gradient: 'linear-gradient(90deg, #000000 0%, rgba(255, 255, 255, 0) 100%)',
+    arrowColor: '#fff',
+    imageMobile: banner1Mobile
+  },
+  {
+    pretitle: '미석 박수근',
+    title: '나무와여인들',
+    titleColor: '#fff',
+    pretitleColor: '#EAEFF3',
+    image: banner2,
+    gradient: 'linear-gradient(90deg, #000000 0%, rgba(255, 255, 255, 0) 100%)',
+    arrowColor: '#fff',
+    imageMobile: banner2Mobile
+  },
+  {
+    pretitle: '이우환',
+    title: '도판',
+    titleColor: '#121212',
+    pretitleColor: '#2D2D2D',
+    image: banner3,
+    arrowColor: '#7D8890',
+    imageMobile: banner3Mobile
   }
-];
+]
 
 const settings = {
   dots: true,
@@ -22,22 +51,63 @@ const settings = {
   speed: 500,
   slidesToShow: 1,
   slidesToScroll: 1,
-  prevArrow: <ArrowBackIosNewRoundedIcon />,
-  nextArrow: <ArrowForwardIosRoundedIcon />
-};
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 5000
+  // prevArrow: <ArrowBackIosNewRoundedIcon />,
+  // nextArrow: <ArrowForwardIosRoundedIcon />
+}
 
 const Hero = () => {
+  const ref = useRef()
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const slideNext = () => {
+    ref.current.slickNext()
+  }
+  const slidePrevious = () => {
+    ref.current.slickPrev()
+  }
+
   return (
     <Box className={classNames(styles.wrapper, 'hero')}>
       <Container className={styles.container} maxWidth>
-        <Slider {...settings}>
+        <Slider
+          {...settings}
+          ref={ref}
+          beforeChange={(currentSlide, nextSlide) => {
+            setCurrentIndex(nextSlide)
+          }}
+        >
           {items.map((item, i) => (
             <CarouselItem key={i} item={item} />
           ))}
         </Slider>
+        <div className={styles.arrows}>
+          <div
+            className={styles.arrow}
+            style={{ borderColor: items[currentIndex].arrowColor }}
+            onClick={slidePrevious}
+          >
+            <ArrowBackIosNewRoundedIcon
+              style={{ color: items[currentIndex].arrowColor }}
+              fontSize='small'
+            />
+          </div>
+          <div
+            className={styles.arrow}
+            style={{ borderColor: items[currentIndex].arrowColor }}
+            onClick={slideNext}
+          >
+            <ArrowForwardIosRoundedIcon
+              style={{ color: items[currentIndex].arrowColor }}
+              fontSize='small'
+            />
+          </div>
+        </div>
       </Container>
     </Box>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
