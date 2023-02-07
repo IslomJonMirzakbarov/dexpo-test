44e913190dd59545fd51dbde1b4d65eba19fd713
otabek <splabdev@rfalab.com>
2023-02-07 12:59:22 +0500

    url changed

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -12,14 +12,14 @@ export default function SEO({
     <>
       {/* Standard metadata tags */}
       <Meta name="description" content={description} />
-      <Meta name="website" content="https://www.dexpo.world" />
+      <Meta name="website" content="http://www.dexpo.world" />
       {/* End standard metadata tags */}
 
       {/* Facebook tags */}
       <Meta property="og:type" content={type} />
       <Meta property="og:title" content={title} />
       <Meta property="og:description" content={description} />
-      <Meta property="og:website" content="https://www.dexpo.world" />
+      <Meta property="og:website" content="http://www.dexpo.world" />
       <Meta
         property="og:image"
         content="https://gateway.pinata.cloud/ipfs/QmTpCKhMz7v8JW2jRTX8oLkd8TuwwbbxbYZKh5Gia1UepX"
@@ -31,7 +31,7 @@ export default function SEO({
       <Meta name="twitter:creator" content={name} />
       <Meta name="twitter:card" content={type} />
       <Meta name="twitter:title" content={title} />
-      <Meta property="twitter:website" content="https://www.dexpo.world" />
+      <Meta property="twitter:website" content="http://www.dexpo.world" />
       <Meta name="twitter:description" content={description} />
       <Meta
         name="twitter:image"
