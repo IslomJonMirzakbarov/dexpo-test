ea38e6383e56e4be2e91ab6201f1428dc3e639b1
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2023-02-13 15:40:14 +0900

    marketplace tabs translated

--- src/components/CTabs/index.jsx
+++ src/components/CTabs/index.jsx
@@ -1,14 +1,16 @@
 import classNames from 'classnames'
+import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 import styles from './style.module.scss'
 
 export default function CTabs({ items, active }) {
+  const { t } = useTranslation()
   return (
     <div className={styles.tabs}>
       {items?.map((item) => (
         <Link to={item.link}>
           <div className={styles.item}>
-            {item.title}
+            {t(item.title)}
             {item.key === active && <div className={classNames(styles.box)} />}
           </div>
         </Link>
