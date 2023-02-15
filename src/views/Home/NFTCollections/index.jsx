407ff45e34920504324acc382a964bf7bf36c727
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2023-02-15 10:34:23 +0900

    sold out switched with hottest artworks for mainnet

--- src/views/Home/NFTCollections/index.jsx
+++ src/views/Home/NFTCollections/index.jsx
@@ -1,47 +1,47 @@
-import { Box, Container, Typography, useMediaQuery } from "@mui/material";
-import React from "react";
-import styles from "./style.module.scss";
-import classNames from "classnames";
-import { useTheme } from "@mui/styles";
-import { useInView } from "react-intersection-observer";
+import { Box, Container, Typography, useMediaQuery } from '@mui/material'
+import React from 'react'
+import styles from './style.module.scss'
+import classNames from 'classnames'
+import { useTheme } from '@mui/styles'
+import { useInView } from 'react-intersection-observer'
 import useCollecionsByCategory, {
   categoryTypes,
-} from "../../../hooks/useCollectionsByCategoryAPI";
-import { CollectionsSuspence } from "./CollectionsContainer";
-import { useTranslation } from "react-i18next";
-import useOriginalNftAPI from "../../../hooks/useOriginalNftAPI";
+} from '../../../hooks/useCollectionsByCategoryAPI'
+import { CollectionsSuspence } from './CollectionsContainer'
+import { useTranslation } from 'react-i18next'
+import useOriginalNftAPI from '../../../hooks/useOriginalNftAPI'
 
-const CollectionsContainer = React.lazy(() => import("./CollectionsContainer")); // Lazy-loaded
+const CollectionsContainer = React.lazy(() => import('./CollectionsContainer')) // Lazy-loaded
 
 const NFTCollections = () => {
   const { ref, inView } = useInView({
     threshold: 0,
-  });
+  })
 
   const { ref: ref1, inView: inView1 } = useInView({
     threshold: 0,
-  });
+  })
 
   const { data: soldOutData, isLoading: isLoadingSoldOut } = useOriginalNftAPI({
     page: 1,
-    search: "",
-    type: "RECENTLY_SOLD",
-  });
-  const soldOutCollections = soldOutData?.items;
+    search: '',
+    type: 'RECENTLY_SOLD',
+  })
+  const soldOutCollections = soldOutData?.items
 
   const { collections: hottestCollections, isLoading: isLoadingHottest } =
-    useCollecionsByCategory(categoryTypes.HOTTEST, null, inView1);
+    useCollecionsByCategory(categoryTypes.HOTTEST, null, inView1)
 
   const { collections: notableCollections, isLoading: isLoadingNotable } =
-    useCollecionsByCategory(categoryTypes.NOTABLE, null, inView);
+    useCollecionsByCategory(categoryTypes.NOTABLE, null, inView)
 
-  const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.down("sm"));
+  const theme = useTheme()
+  const matches = useMediaQuery(theme.breakpoints.down('sm'))
 
-  const { t } = useTranslation();
+  const { t } = useTranslation()
 
   return (
-    <Box className={classNames(styles.container, "collections")}>
+    <Box className={classNames(styles.container, 'collections')}>
       <Container>
         <Box className={styles.block}>
           <Box
@@ -51,20 +51,26 @@ const NFTCollections = () => {
             mb={5}
             ref={ref}
           >
-            <Typography variant="h2" fontWeight={700}>
-              {t("Sold Out Artworks")}
+            {/* Below code will be switched with the above one as soon as items will start sell, so please don't remove it */}
+            {/* <Typography variant="h2" fontWeight={700}>
+              {t('Sold Out Artworks')}
             </Typography>
           </Box>
-          {isLoadingSoldOut || soldOutCollections?.length < 1 ? (
+          {isLoadingSoldOut || soldOutCollections?.length < 1 ? ( */}
+            <Typography variant="h2" fontWeight={700}>
+              {t('HottestArtworks')}
+            </Typography>
+          </Box>
+          {isLoadingHottest || hottestCollections?.length < 1 ? (
             <CollectionsSuspence />
           ) : (
             <CollectionsContainer
-              soldOut
-              collections={soldOutCollections}
+              collections={hottestCollections}
               matches={matches}
             />
           )}
         </Box>
+
         <Box className={styles.block}>
           <Box
             display="flex"
@@ -74,7 +80,7 @@ const NFTCollections = () => {
             ref={ref1}
           >
             <Typography variant="h2" fontWeight={700}>
-              {t("NotableArtworks")}
+              {t('NotableArtworks')}
             </Typography>
           </Box>
           {isLoadingNotable || notableCollections?.length < 1 ? (
@@ -88,7 +94,7 @@ const NFTCollections = () => {
         </Box>
       </Container>
     </Box>
-  );
-};
+  )
+}
 
-export default NFTCollections;
+export default NFTCollections
